b3b141004cea77888cd6aa680904fe3beee83016
Hassaan Muhammad <hassaans299@gmail.com>
2024-03-17 15:23:42 -0700

    some Sentry updates

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,19 @@ import Hero from "./components/Hero"
 import Highlights from "./components/Highlights"
 import Model from "./components/Model"
 
+import * as Sentry from '@sentry/react'
+
 const App=()=> {
+
   return (
    <main className="bg-black">
     <Navbar/>
     <Hero/>
     <Highlights/>
     <Model/>
+    
    </main>
   ) 
 }
 
-export default App
+export default Sentry.withProfiler(App)
