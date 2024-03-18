31af50c8d1ebf80e317b76412587b6b4650c0c18
Hassaan Muhammad <hassaans299@gmail.com>
2024-03-18 13:59:31 -0700

    Completed

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Features from "./components/Features"
 
 import * as Sentry from '@sentry/react'
 import HowItWorks from "./components/HowItWorks"
+import Footer from "./components/Footer"
 
 const App=()=> {
 
@@ -17,6 +18,7 @@ const App=()=> {
     <Model/>
     <Features/>
     <HowItWorks/>
+    <Footer/>
    </main>
   ) 
 }
